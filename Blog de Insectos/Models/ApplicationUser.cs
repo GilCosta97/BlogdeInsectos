bb133bfbce681fccd55a8900f28f53b7e1701ca7
Gil Costa <gilberto.costa.t0127537@edu.atec.pt>
2024-09-02 17:51:05 +0100

    Add project files.

---
+++ Blog de Insectos/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Blog_de_Insectos.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        // Relação, um user tem 1 ou mais posts mas 1 post pertence a 1 user apenas
+
+        public List<Post> Posts { get; set; }
+    }
+}
---
+++ Blog de Insectos/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿namespace Blog_de_Insectos.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? ShortDescription { get; set; }
+        // Relação, um user tem 1 ou mais posts mas 1 post pertence a 1 user apenas
+        public string? ApplicationUserId { get; set; }
+        public ApplicationUser? ApplicationUser { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public string? Description { get; set; }
+        public string? Slug { get; set; }
+    }
+}
