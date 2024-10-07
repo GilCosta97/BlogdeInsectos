f8b56251aa7ce9905f55bd43ec14dea95cbdab96
Gil Costa <gilberto.costa.t0127537@edu.atec.pt>
2024-10-07 19:53:31 +0100

    Video 20

--- Blog de Insectos/Areas/Admin/Controllers/PostController.cs
+++ Blog de Insectos/Areas/Admin/Controllers/PostController.cs
@@ -46,9 +46,9 @@ namespace Blog_de_Insectos.Areas.Admin.Controllers
             {
                 Id = x.Id,
                 Title = x.Title,
+                AuthorName = x.ApplicationUser!.FirstName + " " + x.ApplicationUser!.LastName,
                 CreatedAt = x.CreatedAt,
                 ThumbnailUrl = x.ThumbnailUrl,
-                AuthorName = x.ApplicationUser!.FirstName + " " + x.ApplicationUser!.LastName,
             }).ToList();
 
             return View(listOfPostsVM);
@@ -100,12 +100,66 @@ namespace Blog_de_Insectos.Areas.Admin.Controllers
             var loggedInUserRole = await _userManager.GetRolesAsync(loggedInUser!);
             if (loggedInUserRole[0] == WebsiteRoles.WebsiteAdmin || loggedInUser?.Id == post?.ApplicationUserId)
             {
-                _context.Posts!.Remove(post);
+                _context.Posts!.Remove(post!);
                 await _context.SaveChangesAsync();
                 _notification.Success("Post Deleted Successfully!");
                 return RedirectToAction("Index", "Post", new { area = "Admin" });
             }
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var post = await _context.Posts!.FirstOrDefaultAsync(x => x.Id == id);
+            if (post == null)
+            {
+                _notification.Error("Post not found!");
+                return View();
+            }
+
+            var loggedInUser = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity!.Name);
+            var loggedInUserRole = await _userManager.GetRolesAsync(loggedInUser!);
+            if (loggedInUserRole[0] != WebsiteRoles.WebsiteAdmin && loggedInUser!.Id != post.ApplicationUserId)
+            {
+                _notification.Error("You are not authorized!");
+                return RedirectToAction("Index");
+            }
+
+            var vm = new CreatePostVM()
+            {
+                Id = post.Id,
+                Title = post.Title,
+                ShortDescription = post.ShortDescription,
+                Description = post.Description,
+                ThumbnailUrl = post.ThumbnailUrl,
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(CreatePostVM vm)
+        {
+            if (!ModelState.IsValid) { return View(vm); }
+            var post = await _context.Posts!.FirstOrDefaultAsync(x => x.Id == vm.Id);
+            if (post == null)
+            {
+                _notification.Error("Post not found!");
+                return View();
+            }
+
+            post.Title = vm.Title;
+            post.ShortDescription = vm.ShortDescription;
+            post.ApplicationUserId = vm.ApplicationUserId; 
+            post.Description = vm.Description;
+            if(vm.ThumbnailUrl != null)
+            {
+                post.ThumbnailUrl = UploadImage(vm.Thumbnail!);
+            }
+            await _context.SaveChangesAsync();
+            _notification.Success("Post updated successfully!");
+            return RedirectToAction("Index", "Post", new {area = "Admin"});
         }
 
         private string UploadImage(IFormFile file)
